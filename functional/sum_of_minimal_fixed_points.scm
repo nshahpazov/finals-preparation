5ffab923a06e6c40cae6dd63b917b16baffaa4d8
nshahpazov <nshahpazov@gmail.com>
2018-09-04 19:38:56 +0300

    sum of minimal fixed points

--- functional/sum_of_minimal_fixed_points.scm
+++ functional/sum_of_minimal_fixed_points.scm
@@ -1,5 +1,7 @@
+#lang racket
+
 (define (sumMinFixedPoints l f)
   (apply +
- (map (lambda (el) (foldl min 1000 el))
+ (map (lambda (el) (foldl min (car el) el))
  (map (lambda (y) (filter (lambda (z) (= (f z) z)) y)) l))))
 
--- sum_of_minimal_fixed_points.scm
+++
@@ -1,7 +0,0 @@
-#lang racket
-
-(define (sumMinFixedPoints l f)
-  (apply +
- (map (lambda (el) (foldl min (car el) el))
- (map (lambda (y) (filter (lambda (z) (= (f z) z)) y)) l))))
-
