6cc8062b49860ddb498556a5fdea26bc781af3a0
nshahpazov <nshahpazov@gmail.com>
2018-09-09 12:34:18 +0300

    added a function for summing fixed points for a single list with given a list of functions

--- functional/sum_of_minimal_fixed_points.scm
+++ functional/sum_of_minimal_fixed_points.scm
@@ -2,6 +2,22 @@
 
 (define (sumMinFixedPoints l f)
   (apply +
- (map (lambda (el) (foldl min (car el) el))
- (map (lambda (y) (filter (lambda (z) (= (f z) z)) y)) l))))
+         (map (lambda (el) (foldl min (car el) el))
+              (map (lambda (y) (filter (lambda (z) (= (f z) z)) y)) l))))
 
+; some function which has 2 as a fixed point
+(define (f x)
+  (+ (- (* x x) (* 3 x)) 4))
+
+
+
+(define (smfp numbers functions)
+  (apply + 
+  (map (lambda (l) (foldl min (if (null? l) 0 (car l)) l))
+       (map (lambda (f) (filter (lambda (n) (= n (f n)))
+                           numbers))
+       functions))))
+
+
+
+  
